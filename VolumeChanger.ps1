a75c20a0cc0db88787189b609eafa2f61c970898
inputt_dev <swattedbith@gmail.com>
2026-01-07 20:17:12 +0000

    Update VolumeChanger.ps1

--- VolumeChanger.ps1
+++ VolumeChanger.ps1
@@ -1,2 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
-1..50 | ForEach-Object { [System.Windows.Forms.SendKeys]::SendWait([char]175); Start-Sleep -Milliseconds 10 }
+1..50 | ForEach-Object { 
+    [System.Windows.Forms.SendKeys]::SendWait([char]175)
+    Start-Sleep -Milliseconds (Get-Random -Minimum 5 -Maximum 15)
+}

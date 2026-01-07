4c3b5c68695cbb0ee62a398e1130b156a4774283
inputt_dev <swattedbith@gmail.com>
2026-01-07 20:07:25 +0000

    Create VolumeChanger.ps1

---
+++ VolumeChanger.ps1
@@ -0,0 +1,36 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+function Set-Level {
+    param (
+        [int]$Value,
+        [int]$Pause = 40
+    )
+
+    1..50 | ForEach-Object {
+        [System.Windows.Forms.SendKeys]::SendWait([char]174)
+        Start-Sleep -Milliseconds 5
+    }
+
+    $count = [Math]::Ceiling($Value / 2)
+    1..$count | ForEach-Object {
+        [System.Windows.Forms.SendKeys]::SendWait([char]175)
+        Start-Sleep -Milliseconds $Pause
+    }
+}
+
+$range = 0,10,20,30,40,50,60,70,80,90,100
+
+while ($true) {
+    $sequence = if (Get-Random -Minimum 0 -Maximum 2) {
+        $range
+    } else {
+        $range[::-1]
+    }
+
+    foreach ($step in $sequence) {
+        Set-Level -Value $step -Pause (Get-Random 25 60)
+        Start-Sleep -Milliseconds (Get-Random 200 600)
+    }
+
+    Start-Sleep -Seconds (Get-Random 1 3)
+}
